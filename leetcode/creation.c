7aeaec911061bca9164a60a9b9891d866385819a
amitpattankar01@gmail.com <amitpattankar01@gmail.com>
2026-08-27 19:50:50 +0530

    added creation

---
+++ leetcode/creation.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+
+#include<stdlib.h>
+
+
+
+typedef struct node{
+
+    int data;
+
+    struct node *next;
+
+}n;
+
+
+
+int main()
+
+{
+
+    int  num;
+
+    n *head=NULL;
+
+    n *ptr;
+
+printf("enter number of nodes:");
+
+scanf("%d",&num);
+
+
+
+for(int i=0;i<num;i++){
+
+    n *newnode=(n*)malloc(sizeof(n));
+
+    printf("enter data in node:");
+
+    scanf("%d",&newnode->data);
+
+
+
+    newnode->next=NULL;
+
+
+
+    if(head==NULL){
+
+        head=newnode;
+
+    }
+
+    else{
+
+        ptr=head;
+
+        while(ptr->next!=NULL){
+
+            ptr=ptr->next;
+
+        }
+
+        ptr->next=newnode;
+
+    }
+
+
+
+}
+
+
+
+printf("liked list:\n");
+
+ptr=head;
+
+while(ptr!=NULL){
+
+    printf("%d ->",ptr->data);
+
+    ptr=ptr->next;
+
+}
+
+
+printf("NULL\n");
+}
